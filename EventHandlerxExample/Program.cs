9901429d2c57fa471306200e98b77b330ce46c15
Anton Anton <iamanton45@gmail.com>
2024-02-20 14:22:13 +0000

    Add Observer

---
+++ EventHandlerxExample/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        ProcessBusinessLogic bl = new ProcessBusinessLogic();
+        bl.ProcessCompleted += ProcessCompleted_1; // register with an event
+        bl.ProcessCompleted += ProcessCompleted_2; // register with an event
+        bl.ProcessCompleted -= ProcessCompleted_1; // register with an event
+        bl.StartProcess();
+    }
+
+    // event handler
+    public static void ProcessCompleted_1(object sender, EventArgs e)
+    {
+        Console.WriteLine("Process Completed 1 !");
+    }
+
+    public static void ProcessCompleted_2(object sender, EventArgs e)
+    {
+        Console.WriteLine("Process Completed 2 !");
+    }
+}
+
+public class ProcessBusinessLogic
+{
+    // declaring an event using built-in EventHandler
+    public event EventHandler ProcessCompleted;
+
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started!");
+        // some code here..
+        OnProcessCompleted(EventArgs.Empty); //No event data
+    }
+
+    protected virtual void OnProcessCompleted(EventArgs e)
+    {
+        ProcessCompleted?.Invoke(this, e);
+    }
+}
---
+++ ObserverExample/Contract/ISubject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverExample.Contract
+{
+    public interface ISubject
+    {
+        // Attaches the observer to the publisher.
+        void Attach(IObserver observer);
+
+        // Detaches the observer from the publisher.
+
+        void Detach(IObserver observer);
+
+        // Notifies all observers about the event.
+        void Notify();
+    }
+
+}
