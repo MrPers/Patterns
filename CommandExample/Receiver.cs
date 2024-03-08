50ce17cf8761e488c25dcce9db1d68f8a8a2a7be
Anton Anton <iamanton45@gmail.com>
2024-03-08 13:54:02 +0000

    Edit CommandExample

---
+++ CommandExample/Receiver.cs
@@ -0,0 +1,15 @@
+﻿namespace CommandExample
+{
+    class Receiver
+    {
+        public void DoSomething(string a)
+        {
+            Console.WriteLine($"Receiver: Working on ({a}.)");
+        }
+
+        public void DoSomethingElse(string b)
+        {
+            Console.WriteLine($"Receiver: Also working on ({b}.)");
+        }
+    }
+}
